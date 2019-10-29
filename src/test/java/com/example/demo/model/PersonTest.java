9a27d0d1f43f03d5044dbae32b7991a8085b6df9
fantacucilucas <57140322+fantacucilucas@users.noreply.github.com>
2019-10-29 14:25:00 -0300

    Update PersonTest.java

--- src/test/java/com/example/demo/model/PersonTest.java
+++ src/test/java/com/example/demo/model/PersonTest.java
@@ -7,15 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonTest {
 
     @Test
-    public void givenANewUser_whenCreatingWithValidInputs_thenShouldExecuteSuccessfully(){
-        Person person = new Person("Lucas");
-    }
-
-    @Test
     public void givenANewUser_whenCreatingWithoutAValidName_thenShouldThrownIllegalArgumentException(){
         assertThrows(IllegalArgumentException.class, () -> {
             Person person = new Person("");
         });
     }
-
 }
